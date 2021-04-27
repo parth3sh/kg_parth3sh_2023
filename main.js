8d19420af8ed5279715cff61dbe5bf909419a8dc
Parthesh Patel <parthesh152@gmail.com>
2021-04-27 00:22:11 -0400

    added necessary functionality of printing input integers phonetically

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 function sayNumbers(arguments){
+    var result = "";
     var dict = {
-        1: "One",
-        2: "Two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine"
+        '1': "One",
+        '2': "Two",
+        '3': "Three",
+        '4': "Four",
+        '5': "Five",
+        '6': "Six",
+        '7': "Seven",
+        '8': "Eight",
+        '9': "Nine"
       };
+    for (var i = 2; i < arguments.length; i++){
+        var numString = arguments[i].toString();
+        for (var j = 0; j < numString.length; j++){
+            result = result.concat(dict[numString.charAt(j)]);
+        }
+        if (i != arguments.length - 1){
+            result = result.concat(",");
+        }
+    }
+    console.log(result);
 }
 
+sayNumbers(process.argv);
